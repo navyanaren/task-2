d334221a5f9e45a3b84536bd4c2b2768e7383c92
Navya <navyanarendra12@gmail.com>
2024-02-18 10:23:58 +0530

    added

---
+++ GradeCalculatorTask2/GradeCalculatorTask2.js
@@ -0,0 +1,29 @@
+function calculateGrade(scores, customScale = null) {
+    const defaultScale = {
+      A: 90,
+      B: 80,
+      C: 70,
+      D: 60,
+      F: 0,
+    };
+    const scale = customScale || defaultScale;
+    const averageScore = scores.reduce((sum, score) => sum + score, 0) / scores.length;
+    for (const grade in scale) {
+      if (averageScore >= scale[grade]) {
+        return grade;
+      }
+    }
+  }
+  const inputScores = [78, 90, 82, 99, 65];
+  const finalGrade = calculateGrade(inputScores);
+  console.log(`Final Grade: ${finalGrade}`);
+
+const customScale = {
+    A: 95,
+    B: 85,
+    C: 75,
+    D: 65,
+    F: 0,
+  };
+  const finalGradeCustomScale = calculateGrade(inputScores, customScale);
+  console.log(`Final Grade with Custom Scale: ${finalGradeCustomScale}`);
